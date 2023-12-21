6c2dae5a19bf05172a83ccc195fbe2df5c629aca
Leo Wang <ab0988956087@gmail.com>
2023-12-21 22:01:07 +0800

    feat: allow navigation between screens

--- app/src/main/java/com/example/bluetoothremotecar/MainActivity.kt
+++ app/src/main/java/com/example/bluetoothremotecar/MainActivity.kt
@@ -3,21 +3,29 @@ package com.example.bluetoothremotecar
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.bluetoothremotecar.ui.theme.BluetoothRemoteCarTheme
+
+
+enum class Screen() {
+  Home,
+  Joystick
+}
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Text("Hello World")
-        }
         setContent {
             BluetoothRemoteCarTheme {
                 // A surface container using the 'background' color from the theme
@@ -25,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    AppNavigation()
                 }
             }
         }
@@ -33,16 +41,34 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-    )
+fun AppNavigation() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Screen.Home.name) {
+        composable(Screen.Home.name) {
+            HomeScreen(navController = navController)
+        }
+        composable(Screen.Joystick.name) {
+            JoystickScreen(navController = navController)
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    BluetoothRemoteCarTheme {
-        Greeting("Android")
+fun HomeScreen(navController: NavController){
+        Column {
+        Text(text = "Home Screen")
+        Button(onClick = { navController.navigate(Screen.Joystick.name) }) {
+            Text(text = "Go to Screen B")
+        }
+    }
+}
+
+@Composable
+fun JoystickScreen(navController: NavController){
+    Column {
+        Text(text = "Screen Joystick")
+        Button(onClick = { navController.navigate(Screen.Home.name) }) {
+            Text(text = "Go to Screen A")
+        }
     }
 }
